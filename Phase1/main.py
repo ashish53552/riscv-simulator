814a70e14d617a553b572417976162c33878fa21
Hrishikesh <pawarhrishi21@gmail.com>
2021-03-29 00:14:25 +0530

    bin conv for instruction done

--- Phase1/main.py
+++ Phase1/main.py
@@ -1,10 +1,15 @@
 #instructions_machine_code array will have all the .mc file instructions
-instrutions_machine_code = []
+instructions_machine_code = []
+
 input_file = open("./test/input.mc","r")
 
 for line in input_file:
-    instrutions_machine_code.append(line)
-#currentInstruction dictionary holds values for the instruction currently getting executed
+    instructions_machine_code.append(line)
+
+#total instructions in .mc file
+no_of_instructions = len(instructions_machine_code)
+
+#current_instruction dictionary holds values for the instruction currently getting executed
 current_instruction = {
     "opcode":"",
     "funct3":"",
@@ -14,5 +19,14 @@ current_instruction = {
     "rs2":"",
     "rd":"",
 }
+
+##Currently solving assuming only one instruction in .mc file, later adding a for loop would make the program work for .mc file with multiple instructions
+for instruction in instructions_machine_code:
+    #split into instruction number and actual instruction (see .mc file to understand)
+    split_instruction = instruction.split()
+  #  print(bin(int(split_instruction[1],16)))
+    print(format(int(split_instruction[1],16), '0>32b'))
+    #bin_instruction = 
+
 
 
