c547af5a0a12d51bad7e6b0da8858313b0b8e833
Hrishikesh <pawarhrishi21@gmail.com>
2021-03-29 00:43:25 +0530

    implement opcode branching

--- Phase1/main.py
+++ Phase1/main.py
@@ -1,3 +1,5 @@
+
+
 #instructions_machine_code array will have all the .mc file instructions
 instructions_machine_code = []
 
@@ -9,24 +11,28 @@ for line in input_file:
 #total instructions in .mc file
 no_of_instructions = len(instructions_machine_code)
 
-#current_instruction dictionary holds values for the instruction currently getting executed
-current_instruction = {
-    "opcode":"",
-    "funct3":"",
-    "funct7":"",
-    "imm":"",
-    "rs1":"",
-    "rs2":"",
-    "rd":"",
-}
+
 
 ##Currently solving assuming only one instruction in .mc file, later adding a for loop would make the program work for .mc file with multiple instructions
 for instruction in instructions_machine_code:
-    #split into instruction number and actual instruction (see .mc file to understand)
-    split_instruction = instruction.split()
-  #  print(bin(int(split_instruction[1],16)))
-    print(format(int(split_instruction[1],16), '0>32b'))
-    #bin_instruction = 
+
+    split_instruction = instruction.split()     #split into instruction number and actual instruction (see .mc file to understand)
+    bin_instruction = format(int(split_instruction[1],16), '0>32b')     #bin_instruction is string with instruction in 32bit binary form
+
+
+    print(bin_instruction[0:7])
+
+    #current_instruction dictionary holds values for the instruction currently getting executed
+    current_instruction = {
+        "opcode":"",
+        "funct3":"",
+        "funct7":"",
+        "imm":"",
+        "rs1":"",
+        "rs2":"",
+        "rd":"",
+    }
+
 
 
 
