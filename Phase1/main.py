87c6dd21b770079ca71cceecff4e0805a60e7221
Hrishikesh <pawarhrishi21@gmail.com>
2021-03-29 14:30:59 +0530

    update main : extract fields from ins_enc

--- Phase1/main.py
+++ Phase1/main.py
@@ -1,3 +1,5 @@
+from instruction_encoding import *
+
 #instructions_machine_code array will have all the .mc file instructions
 instructions_machine_code = []
 
@@ -10,53 +12,34 @@ for line in input_file:
 no_of_instructions = len(instructions_machine_code)
 
 
-
 for instruction in instructions_machine_code:
 
     split_instruction = instruction.split()     #split into instruction number and actual instruction (see .mc file to understand)
     bin_instruction = format(int(split_instruction[1],16), '0>32b')     #bin_instruction is string with instruction in 32bit binary form
 
     current_instruction_opcode = bin_instruction[0:7] #opcode extracted
+    extracted_fields = {}
+    if current_instruction_opcode=='0110011':         #R-format
+        extracted_fields = extract_R_type(instruction) 
 
-    if current_instruction_opcode=='0110011':
-        #R-format
-        current_instruction_rd = bin_instruction[7:12]
-        current_instruction_funct3 = bin_instruction[12:15]
-        current_instruction_rs1 = bin_instruction[15:20]
-        current_instruction_rs2 = bin_instruction[20:25]
-        current_instruction_funct7 = bin_instruction[25:]
+    elif current_instruction_opcode=='0000011':         #I-format
+        extracted_fields = extract_I_type(instruction)
+    
+    elif current_instruction_opcode=='0100011':        #S-format
+        extracted_fields = extract_S_type(instruction)
+    
+    elif current_instruction_opcode=='1101111':        #UJ format
+        extracted_fields = extract_UJ_type(instruction)
+    
+    elif current_instruction_opcode=='1100011':        #SB format
+        extracted_fields = extract_SB_type(instruction)
 
-     #current_instruction dictionary holds values for the instruction currently getting executed
-        current_instruction = {
-            "opcode":current_instruction_opcode,
-            "funct3":current_instruction_funct3,
-            "funct7":current_instruction_funct7,
-            "imm":"",
-            "rs1":current_instruction_rs1,
-            "rs2":current_instruction_rs2,
-            "rd":current_instruction_rd,
-        }       
-
-    elif current_instruction_opcode=='0000011': #Note - 0010011 also is I format
-        #I-format
-        print()
-    
-    elif current_instruction_opcode=='0100011':
-        #S-format
-        print()
-    
-    elif current_instruction_opcode=='1101111':
-        #UJ format
-        print()
-    
-    elif current_instruction_opcode=='1100011':
-        #SB format
-        print()
+    elif current_instruction_opcode=='0010111':        #U format
+        extracted_fields = extract_U_type(instruction)
 
     else:
         print("INVALID OPCODE\n")
 
-    #current_instruction dictionary will be passed further
 
 
 
