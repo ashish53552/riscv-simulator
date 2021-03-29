5588444ddfef61d50313e4aa53b8d8bd35ff2299
divyansh2111 <divsriva@gmail.com>
2021-03-29 13:17:49 +0530

    updated R, S and U types

--- Phase1/instruction_encoding.py
+++ Phase1/instruction_encoding.py
@@ -2,10 +2,26 @@
 #The final extracted bits will be read into decimal values as a dictionary given below
 #The variable istruction is a string of the 32 bit binary instruction
 
-
+# ASSUMING INSTRUCTION OF TYPE 0x0 0x11111111
 def extract_R_type(instruction) :
 
-	extracted_fields = {"opc_code" : None, "imm" : None, "funct3" : None, "funct7" : None, "rs1" : None, "rs2" : None, "rd" : None}
+	extracted_fields = {"opc_code" : None,
+						"imm" : None,
+						"funct3" : None,
+						"funct7" : None,
+						"rs1" : None,
+						"rs2" : None,
+						"rd" : None}
+
+	# Extracting the main part of the instruction and converting from hex to binary
+	bin_instruction = format(int(instruction.split()[1], 16), '0>32b')
+
+	extracted_fields['opc_code'] = bin_instruction[25:]
+	extracted_fields['rd'] = bin_instruction[20:25]
+	extracted_fields['funct3'] = bin_instruction[17:20]
+	extracted_fields['rs1'] = bin_instruction[12:17]
+	extracted_fields['rs2'] = bin_instruction[7:12]
+	extracted_fields['funct7'] = bin_instruction[:7]
 
 	return extracted_fields ;
 
@@ -19,7 +35,22 @@ def extract_I_type(instruction) :
 
 def extract_S_type(instruction) :
 
-	extracted_fields = {"opc_code" : None, "imm" : None, "funct3" : None, "funct7" : None, "rs1" : None, "rs2" : None, "rd" : None}
+	extracted_fields = {"opc_code" : None,
+						"imm" : None,
+						"funct3" : None,
+						"funct7" : None,
+						"rs1" : None,
+						"rs2" : None,
+						"rd" : None}
+
+	# Extracting the main part of the instruction and converting from hex to binary
+	bin_instruction = format(int(instruction.split()[1], 16), '0>32b')
+
+	extracted_fields['opc_code'] = bin_instruction[25:]
+	extracted_fields['imm'] = bin_instruction[:7] + bin_instruction[20:25]
+	extracted_fields['funct3'] = bin_instruction[17:20]
+	extracted_fields['rs1'] = bin_instruction[12:17]
+	extracted_fields['rs2'] = bin_instruction[7:12]
 
 	return extracted_fields ;
 
@@ -40,6 +71,19 @@ def extract_UJ_type(instruction) :
 
 def extract_U_type(instruction) :
 
-	extracted_fields = {"opc_code" : None, "imm" : None, "funct3" : None, "funct7" : None, "rs1" : None, "rs2" : None, "rd" : None}
+	extracted_fields = {"opc_code" : None,
+						"imm" : None,
+						"funct3" : None,
+						"funct7" : None,
+						"rs1" : None,
+						"rs2" : None,
+						"rd" : None}
+
+	# Extracting the main part of the instruction and converting from hex to binary
+	bin_instruction = format(int(instruction.split()[1], 16), '0>32b')
+
+	extracted_fields['opc_code'] = bin_instruction[25:]
+	extracted_fields['rd'] = bin_instruction[20:25]
+	extracted_fields['imm'] = bin_instruction[:20]
 
 	return extracted_fields ;
