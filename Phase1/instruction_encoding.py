f734cc5bf8b9c1295331d49450c870d7a9a58b96
ashish53552 <50573312+ashish53552@users.noreply.github.com>
2021-03-29 13:45:06 +0530

    updated I type

--- Phase1/instruction_encoding.py
+++ Phase1/instruction_encoding.py
@@ -28,9 +28,18 @@ def extract_R_type(instruction) :
 
 def extract_I_type(instruction) :
 
+	#same as r type instruction
 	extracted_fields = {"opc_code" : None, "imm" : None, "funct3" : None, "funct7" : None, "rs1" : None, "rs2" : None, "rd" : None}
 
-	return extracted_fields ;
+	bin_instruction = format(int(instruction.split()[1], 16), '0>32b')
+
+	extracted_fields['opc_code'] = bin_instruction[25:]
+	extracted_fields['rd'] = bin_instruction[20:25]
+	extracted_fields['funct3'] = bin_instruction[17:20]
+	extracted_fields['rs1'] = bin_instruction[12:17]
+	extracted_fields['imm'] = bin_instruction[:12]
+
+	return extracted_fields 
 
 
 def extract_S_type(instruction) :
