7f8e149d6c7b19b9bb4a1c9382bdd0a0ea57d69a
divyansh2111 <divsriva@gmail.com>
2021-03-29 14:51:30 +0530

    SB Format Updated

--- Phase1/instruction_encoding.py
+++ Phase1/instruction_encoding.py
@@ -78,6 +78,21 @@ def extract_SB_type(bin_instruction) :
 		"rs2" : None,
 		"rd" : None,
 	}
+
+	# SB format - opcode - [6:0] , f3-[12:14] , rs1 - [15:19] , rs2 - [20:24]  , imm - [7:10(1:4bits), 25:30(5:10bits), 11(11th bit) ,31(12th bit)]
+
+	bin_instruction = format(int(instruction.split()[1], 16), '0>32b')
+	extracted_fields["opc_code"] = bin_instruction[25:]
+	extracted_fields["rs1"] = bin_instruction[12:17]
+	extracted_fields["rs2"] = bin_instruction[7:12]
+	extracted_fields["funct3"] = bin_instruction[17:20]
+	imm1 = bin_instruction[21:25]
+	imm2 = bin_instruction[1:7]
+	imm3 = bin_instruction[20]
+	imm4 = bin_instruction[0]
+	imm = imm4 + imm3 + imm2 + imm1
+	extracted_fields["imm"] = imm
+	
 	return extracted_fields 
 
 
